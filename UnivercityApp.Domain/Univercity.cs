34defa00afbca7d9508b8c73327428aa8037aa9a
MazenHesham-coder <79680616+MazenHesham-coder@users.noreply.github.com>
2021-04-19 01:37:11 +0200

    Add files via upload

---
+++ UnivercityApp.Domain/Univercity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnivercityApp.domain
+{
+    public class Univercity
+    {
+        
+        // Constructor
+        public Univercity()
+        {
+            Students = new List<Student>();
+        }
+        /**/
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public List<Student> Students { get; set; }
+        
+
+    }
+}
